af028f51bb736f121c8c8c32a795983eff0ed0a6
Eyüp Bayçöl <P1485@DETAY.local>
2020-01-22 23:20:45 +0300

    mLap üzerinden database bağlantısı yapıldı schema oluşturuldu

---
+++ helper/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+  mongoose.connect(
+    "mongodb://movie_user:abcd1234@ds211829.mlab.com:11829/heroku_61qc81fq"
+  );
+  mongoose.connection.on("open", () => {
+    console.log("mongodb connected");
+  });
+  mongoose.connection.on("error", err => {
+    console.log("mongodb error");
+  });
+};
