496ce1d487fb79a2ea88404c941d81b0f3469eb5
Eyüp Bayçöl <P1485@DETAY.local>
2020-02-01 16:52:27 +0300

    Director modeli oluşturuldu. Director ekleme endpointi yazıldı

---
+++ routes/director.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+//models
+
+const Director = require("../models/Director");
+router.post("/", (req, res, next) => {
+  const director = new Director(req.body);
+  var promise = director.save();
+
+  promise
+    .then(data => {
+      if (!data) next({ errormesage: "Uppps Error" });
+
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
